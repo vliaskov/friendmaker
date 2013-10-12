c1432f15ab88574c913f1b53fb47c7b8cab8909f
Vasilis Liaskovitis <vliaskov@gmail.com>
2013-10-12 23:14:00 +0200

    add strangers movement / avoiding

--- main.lua
+++ main.lua
@@ -2,7 +2,9 @@
 -- Add an image to the game and move it around using
 -- the arrow keys.
 -- compatible with löve 0.6.0 and up
-
+numstrangers = 4
+stranger_mindist = 50
+stranger_speed = 20
 function love.load()
    -- love.graphics.setMode(600, 600, false, false, 0)
    hamster = love.graphics.newImage("goblin.gif")
@@ -15,7 +17,7 @@ function love.load()
    math.randomseed( os.time() )
    local i
    strangers = {}
-   for i=1,4,1 do
+   for i=1,numstrangers,1 do
      strangers[i] = {}
      strangers[i].x = {}
      strangers[i].y = {}
@@ -25,6 +27,21 @@ function love.load()
 end
 
 function love.update(dt)
+   for i=1,numstrangers,1 do
+     if ((math.abs(x - strangers[i].x) <stranger_mindist) and (math.abs(y - strangers[i].y) < stranger_mindist)) then
+        if ( (strangers[i].x > x) and (strangers[i].x < width - stranger_speed) ) then
+          strangers[i].x = strangers[i].x + stranger_speed
+        elseif ( (strangers[i].x < x) and (strangers[i].x > stranger_speed) ) then
+          strangers[i].x = strangers[i].x - stranger_speed
+        end
+     if ( (strangers[i].y > y) and (strangers[i].y < height - stranger_speed) ) then
+          strangers[i].y = strangers[i].y + stranger_speed
+        elseif ( (strangers[i].y < y) and (strangers[i].y > stranger_speed) ) then
+          strangers[i].y = strangers[i].y - stranger_speed
+        end
+     end    
+   end
+
    if love.keyboard.isDown("right") then
       x = x + (speed * dt)
    end
@@ -38,14 +55,11 @@ function love.update(dt)
    if love.keyboard.isDown("up") then
       y = y - (speed * dt)
    end
-   if love.keyboard.isDown("space") then
-      y = y - (speed * dt)
-   end
 end
 
 function love.draw()
    love.graphics.draw(hamster, x, y)
-   for i=1,4,1 do
+   for i=1,numstrangers,1 do
     love.graphics.draw(stranger, strangers[i].x, strangers[i].y, 0, 0.3, 0.3)
    end
 end
